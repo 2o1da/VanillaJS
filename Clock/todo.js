990159f85cb857b5fc4767371d4a75d3ef51ac13
solda <solda@khu.ac.kr>
2021-04-20 19:46:44 +0900

    updated

--- Clock/todo.js
+++ Clock/todo.js
@@ -4,21 +4,51 @@ const toDoForm = document.querySelector(".js-toDoForm"),
 
 const TODOS_LS = "toDos";
 
+let toDos = [];
+
+function deleteToDo(event) {
+    const btn = event.target;
+    const li = btn.parentNode;
+    toDoList.removeChild(li);
+    //console.log(event.target.parentNode);
+    const cleanToDos = toDos.filter(function (toDo) {
+        return toDo.id !== parseInt(li.id);
+    }); // ≒forEach(function)
+    // filter:array의 모든 item을 통해 함수 실행하고 true인 itme만 가지고 새로운 array 생성
+    toDos = cleanToDos; // let
+    saveToDos();
+}
+
+function saveToDos() {
+    localStorage.setItem(TODOS_LS, JSON.stringify(toDos)); // LS에 JS의 data 저장할 수 없고 string만 가능
+}
+
 function paintToDo(text) {
     const li = document.createElement("li");
     const delBtn = document.createElement("button");
     const span = document.createElement("span");
+    const newId = toDos.length + 1;
 
     delBtn.innerText = "❌";
+    delBtn.addEventListener("click", deleteToDo);
     span.innerText = text;
 
+    li.id = newId; // <li id="1">
     li.appendChild(span);
     li.appendChild(delBtn);
     toDoList.appendChild(li);
+
+    const toDoObj = {
+        text: text,
+        id: newId,
+    };
+    toDos.push(toDoObj); // 배열 추가
+
+    saveToDos();
 }
 
 function handleSubmit(event) {
-    //event.preventDefault(); // event 금지 방지
+    event.preventDefault(); // event 금지 방지
 
     const currentValue = toDoInput.value;
     paintToDo(currentValue);
@@ -26,9 +56,14 @@ function handleSubmit(event) {
 }
 
 function loadToDos() {
-    const toDos = localStorage.getItem(TODOS_LS); // ???
+    const loadedToDos = localStorage.getItem(TODOS_LS); // ???
 
-    if (toDos !== null) {
+    if (loadedToDos !== null) {
+        const parseToDos = JSON.parse(loadedToDos); // object
+        parseToDos.forEach(function (toDo) {
+            // forEach:array function
+            paintToDo(toDo.text);
+        });
     }
 }
 
